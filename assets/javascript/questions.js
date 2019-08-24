f39909608a4c7e37d38c0332c168a92b641f2992
Jacob Lewis <jacoblewis@Jacobs-MacBook-Pro.local>
2019-08-24 16:22:58 -0700

    Finished the html and made an array of objects which contains the questions for my game.

---
+++ assets/javascript/questions.js
@@ -0,0 +1,37 @@
+var quizQuestions = [
+    {
+        question: "How many games are played in the regular season?",
+        choices: [110, 150, 100, 162],
+        correctAnswer: 162
+    },
+    {
+        question: "The World Series is the championship Title in baseball, which team has won the most?",
+        choices: ["Yankees", "Red Sox", "White Sox", "Tigers"],
+        correctAnswer: "Yankees"
+    },
+    {
+        question: "Arguably the most famous name in baseball, Bath Ruth's real name is?",
+        choices: ["George Herman Ruth Jr.", "Hank Ruth", "Sandy Ruth", "George Brett Ruth"],
+        correctAnswer: "George Herman Ruth Jr."
+    },
+    {
+        question: "The highest number of wins in a single season ever recorded is?",
+        choices: [110, 116, 98, 121],
+        correctAnswer: 116
+    },
+    {
+        question: "When a batter hits a single, double, triple, and homerun all in the same game this is called hitting for the: ",
+        choices: ["Cycle", "Trifecta", "Triple Crown", "Perfection"],
+        correctAnswer: "Cycle"
+    },
+    {
+        question: "In 1970, pitch Doc Ellis claimed to have thrown a no-hitter while under the influence of what drug?",
+        choices: ["LSD", "Cocaine", "Codine", "Marijuana"],
+        correctAnswer: "LSD"
+    },
+    {
+        question: "Pitcher Joel Zumaya missed three games in 2006 citing his injuring from playing this videogame: ",
+        choices: ["Buck Hunter", "Guitar Hero", "Dance Dance Revolution", "Call of Duty"],
+        correctAnswer: "Guitar Hero"
+    }
+];
